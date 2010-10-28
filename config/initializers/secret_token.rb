28b5a051a369caf7d9a28bda9d491fc3ceb5c485
Ben Roesch <bcroesch@gmail.com>
2010-10-27 19:27:16 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Listolicious::Application.config.secret_token = '23ea63a9dc7a13d2003df3085058c64bda5e8f08a87917f2320519333a7930f777be81335ae817144c57c9fc75fec581ae3b9078d0ef0f673c901f6239e4e30a'
